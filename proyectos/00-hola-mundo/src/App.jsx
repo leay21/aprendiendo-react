ddc6328a1bbf398a3b6fb2ffad7293474d925398
Leay21 <yaelt143@gmail.com>
2024-05-07 08:20:04 -0600

    primer commit

---
+++ proyectos/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export function App () {
+    return (
+
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats?ttl=1h" alt="imagen de usuario" />
+                <div>
+                    <strong>Yael Toral Alvarez</strong>
+                    <span>@yaelt20</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )  
+}
